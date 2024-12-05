999af98f32871ff3d4f6db09b4b7233b4252fd54
Bruno Mazziero <bruno.mazziero98@gmail.com>
2024-12-05 10:45:16 -0300

    Clients List

--- FitZonePro/src/main/java/fit_zone/dates/ClientDAO.java
+++ FitZonePro/src/main/java/fit_zone/dates/ClientDAO.java
@@ -1,13 +1,53 @@
 package fit_zone.dates;
 
+import fit_zone.connection.Connect;
 import fit_zone.entity.Client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static fit_zone.connection.Connect.getConnect;
 
 public class ClientDAO implements iClientDAO{
+
+    Scanner read = new Scanner(System.in);
+    boolean flag = false;
+
     @Override
     public List<Client> clientList() {
-        return List.of();
+        List<Client> clients = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection conn = getConnect();
+        String query = "SELECT * FROM client ORDER BY idclient";
+        try {
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                //It has to be inside the while loop because it creates a new instance in memory every time it iterates.
+                Client client = new Client();
+                client.setId(rs.getInt("idclient"));
+                client.setName(rs.getString("name"));
+                client.setSurName(rs.getString("surname"));
+                client.setMemberNumber(rs.getInt("member"));
+                clients.add(client);
+            }
+        } catch (Exception e) {
+            System.out.println("Error to list users " + e.getMessage());
+        }
+        finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Error to close data base " + e.getMessage());
+            }
+        }
+        return clients;
     }
 
     @Override
