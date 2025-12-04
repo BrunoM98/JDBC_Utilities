338445fb8d8ea3de44fc9812a6a9ea9ed4ecb346
Bruno Mazziero <bruno.mazziero98@gmail.com>
2025-12-04 11:23:28 -0300

    act

--- src/main/java/Form.java
+++ src/main/java/Form.java
@@ -1,5 +1,27 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
 import javax.swing.*;
 
-public class Form {
+public class Form extends JFrame{
     private JPanel panel1;
+
+    public Form() {
+        initilizingForm();
+    }
+    private void initilizingForm(){
+
+        setContentPane(panel1);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null); // center
+
+    }
+
+    public static void main(String[] args) {
+
+        FlatDarculaLaf.setup(); // change dark mode
+        Form form = new Form();
+        form.setVisible(true);
+
+    }
+
 }
