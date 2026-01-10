55896a271b6a49f155f508ff6df20f28aeea17b6
Bruno Mazziero <bruno.mazziero98@gmail.com>
2026-01-10 20:46:10 -0300

    Creacion de Entiad y templates

---
+++ contacts/src/main/java/bm/contacts/entity/Contact.java
@@ -0,0 +1,17 @@
+package bm.contacts.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Contact {
+
+    
+}
