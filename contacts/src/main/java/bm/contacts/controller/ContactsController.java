bf56ac85b3eedc044a3da303ebb4fbd4f12df3e5
Bruno Mazziero <bruno.mazziero98@gmail.com>
2026-01-15 12:54:47 -0300

    Implementacion de boiton edit (Error) falta arreglar

--- contacts/src/main/java/bm/contacts/controller/ContactsController.java
+++ contacts/src/main/java/bm/contacts/controller/ContactsController.java
@@ -1,5 +1,6 @@
 package bm.contacts.controller;
 
+import bm.contacts.ContactsApplication;
 import bm.contacts.entity.Contact;
 import bm.contacts.service.ContactService;
 import org.slf4j.Logger;
@@ -7,6 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -30,8 +34,21 @@ public class ContactsController {
     }
     @GetMapping("/add")
     public String addContact(){
-        Contact contact = new Contact();
         return "add";
+    }
+
+    @PostMapping("/add")
+    public String addC(@ModelAttribute("contactForm") Contact contact){
+        logger.info("Contact Add{}", contact);
+        contactService.saveContact(contact);
+        return "redirect:/"; // redireccionamos al path de inicio
+    }
+    @GetMapping("/edit")
+    public String editC(@PathVariable(value = "id")int idContact ,ModelMap modelMap){
+        Contact contact = contactService.searchContactID(idContact);
+        logger.info("Contact Edit{}", contact);
+        modelMap.addAttribute("contact",contact);
+        return "edit";
     }
 
 }
